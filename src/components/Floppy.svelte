2612f561f22a55a8524c5f91dd92cfd4f12da833
Benjamin Baer <benjamin.baer@math.uzh.ch>
2025-08-05 03:19:41 +0200

    made an all css floppy

--- src/components/Floppy.svelte
+++ src/components/Floppy.svelte
@@ -4,32 +4,36 @@
     let cart = "bg-gray-600"
 </script>
 
-<div class="floppy aspect-90/94 w-80 rounded-md shadow-lg">
+<div class="floppy aspect-90/94 w-80">
     <div class="leftside">
-        <div class="row-1 col-span-full {floppy}"></div>
+        <div class="row-1 col-span-full {floppy} rounded-tl-lg"></div>
         <div class="row-2 col-1 {floppy}"></div>
         <div class="row-2 col-3 {floppy}"></div>
         <div class="row-3 col-span-full {floppy}"></div>
     </div>
-    <div class="etiquette"></div>
+    <div class="etiquette inset-shadow-sm/25"></div>
     <div class="rightside">
-        <div class="row-1 col-span-full {floppy}"></div>
+        <div class="row-1 col-span-full {floppy} rounded-tr-lg"></div>
         <div class="row-2 col-1 {floppy}"></div>
+        <div class="row-2 col-2 inset-shadow-sm/30 {darkFloppy}"></div>
         <div class="row-2 col-3 {floppy}"></div>
         <div class="row-3 col-span-full {floppy}"></div>
     </div>
     <div class="row-2 col-span-full {floppy}"></div>
-    <div class="row-3 col-1 {floppy}"></div>
-    <div class="row-3 col-2 cart">
+    <div class="row-3 col-1 leftcorner">
+        <div class="row-1 col-1 {floppy}"></div>
+        <div class="row-2 col-1 {floppy} cut"></div>
+    </div>
+    <div class="row-3 col-2 cart {floppy}">
         <div class="row-span-full col-1 {floppy}"></div>
-        <div class="row-1 col-start-2 col-end-5 {cart}"></div>
+        <div class="row-1 col-start-2 col-end-5 rounded-tl-lg {cart}"></div>
         <div class="row-2 col-2 {cart}"></div>
-        <div class="row-2 col-3 {floppy}"></div>
+        <div class="row-2 col-3 inset-shadow-sm/50 {darkFloppy}"></div>
         <div class="row-2 col-4 {cart}"></div>
         <div class="row-3 col-start-2 col-end-5 {cart}"></div>
-        <div class="row-span-full col-5 {darkFloppy}"></div>
+        <div class="row-span-full inset-shadow-sm/30 rounded-tr-lg col-5 {darkFloppy}"></div>
     </div>
-    <div class="row-3 col-3 {floppy}"></div>
+    <div class="row-3 col-3 {floppy} rounded-br-lg"></div>
 </div>
 
 
@@ -51,10 +55,22 @@
         background-color:white;
     }
 
+    div.cut {
+        /* to create / play around with polygon: https://developer.mozilla.org/en-US/docs/Web/CSS/basic-shape/polygon */
+        /*clip-path: polygon(0% 0%, 100% 100%, 100% 0%); 45 degree cut */
+        clip-path: polygon(0% 30%, 0% 0%, 100% 0%, 100% 100%, 70% 100%);
+    }
+
     div.cart {
         display: grid;
         grid-template-columns: 15% 10% 18% auto 25%; 
         grid-template-rows: 8% auto 10%;
+    }
+
+    div.leftcorner {
+        display: grid;
+        grid-template-columns: auto;
+        grid-template-rows: auto 30%;
     }
 
     div.leftside {
