65527bfb48654b038aedbade364b55af4a1bc347
Benjamin Baer <benjamin.baer@math.uzh.ch>
2025-08-06 00:00:32 +0200

    added lucia for login

---
+++ src/components/Footer.svelte
@@ -0,0 +1,3 @@
+<footer>
+        <p class="text-right text-gray-400">Made with ❤️</p>
+</footer>
---
+++ src/components/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+    import LightButton from "./LightButton.svelte";
+</script>
+
+<LightButton />
+<header class="py-20 px-50">
+
+        
+        <h1>Defy Channel</h1>
+
+        <p class="py-20">Welcome to our little page.</p>
+
+</header>
